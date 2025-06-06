8f4c49ac56bffc0b77b4b7176478ba2bec939e40
matthew-salter <msalter@panelitix.ai>
2025-06-06 11:41:52 +0100

    Update move_files_1.py

--- Scripts/Predictive_Report/move_files_1.py
+++ Scripts/Predictive_Report/move_files_1.py
@@ -5,7 +5,6 @@ from logger import logger
 
 SUPABASE_URL = os.getenv("SUPABASE_URL")
 SUPABASE_BUCKET = "panelitix"
-SUPABASE_ROOT_FOLDER = os.getenv("SUPABASE_ROOT_FOLDER")
 
 def move_supabase_file(from_path, to_path, skipped_files):
     headers = get_supabase_headers()
